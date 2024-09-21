058a48f7a2fd75439d3c89db94b1927f9287af2b
nightfall <akka-bilal@outlook.com>
2024-09-21 18:36:38 +0100

    added color and corrected name of file

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -7,7 +7,7 @@ class AkColors {
   static const Color primary = Color(0xFF4b68ff);
   static const Color secondary = Color(0xFFFFE24B);
   static const Color accent = Color(0xFFb0c7ff);
-
+  static const Color essential = Color(0xFFEFE7DC);
   // Text colors
   static const Color textPrimary = Color(0xFF333333);
   static const Color textSecondary = Color(0xFF6C757D);
