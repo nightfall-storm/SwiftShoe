11bb99d19cc9d6ae858c4dbb3e1d798cec8d9182
nightfall <akka-bilal@outlook.com>
2024-09-22 19:16:45 +0100

    optimized imports

--- lib/features/shop/screens/home/widgets/banner_slider.dart
+++ lib/features/shop/screens/home/widgets/banner_slider.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../../../common/widgets/custom_shapes/containers/circular_container.dart';
-import '../../../../../common/widgets/images/round_images.dart';
+import '../../../../../common/widgets/images/round_image.dart';
 import '../../../../../utils/constants/colors.dart';
 import '../../../../../utils/constants/sizes.dart';
 import '../../../controllers/home_controller.dart';
