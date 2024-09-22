56753004ea4ed509bcf236611038f25c0ffecb17
nightfall <akka-bilal@outlook.com>
2024-09-22 19:18:01 +0100

    reusable circularImage

---
+++ lib/common/widgets/images/circular_image.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants/colors.dart';
+import '../../../utils/constants/image_strings.dart';
+import '../../../utils/constants/sizes.dart';
+import '../../../utils/helpers/helper_functions.dart';
+
+class AkCircularImage extends StatelessWidget {
+  const AkCircularImage({
+    super.key,
+    this.fit,
+    required this.image,
+    this.isNetworkImage = false,
+    this.overlayColor,
+    this.backgroundColor,
+    this.width = 56,
+    this.height = 56,
+    this.padding = AkSizes.sm,
+  });
+  final BoxFit? fit;
+  final String image;
+  final bool isNetworkImage;
+  final Color? overlayColor;
+  final Color? backgroundColor;
+  final double width, height, padding;
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isDarkMode = AkHelperFunctions.isDarkMode(context);
+    return Container(
+      width: width,
+      height: height,
+      padding: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        color:
+            backgroundColor ?? (isDarkMode ? AkColors.black : AkColors.white),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Center(
+        child: Image(
+          fit: fit,
+          image: isNetworkImage
+              ? NetworkImage(image)
+              : AssetImage(image) as ImageProvider,
+          color: overlayColor ?? (isDarkMode ? AkColors.white : AkColors.black),
+        ),
+      ),
+    );
+  }
+}
