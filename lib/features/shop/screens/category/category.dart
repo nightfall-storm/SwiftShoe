5210bc477b214a3f8bb4a2a3f416fd75a7a45bb1
nightfall <akka-bilal@outlook.com>
2024-09-23 18:14:12 +0100

    optimized and fixed tabbar

--- lib/features/shop/screens/category/category.dart
+++ lib/features/shop/screens/category/category.dart
@@ -9,94 +9,107 @@ import 'package:shoes_store/utils/constants/enums.dart';
 import 'package:shoes_store/utils/constants/sizes.dart';
 import 'package:shoes_store/utils/helpers/helper_functions.dart';
 
+import '../../../../common/widgets/appbar/tabbar.dart';
+import '../../../../common/widgets/brand/brand_show_case.dart';
 import '../../../../common/widgets/containers/section_cotainer.dart';
 import '../../../../common/widgets/images/circular_image.dart';
+import '../../../../common/widgets/brand/brand_card.dart';
 import '../../../../utils/constants/colors.dart';
 import '../../../../utils/constants/image_strings.dart';
+import 'widgets/category_tab.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AkAppBar(
-        showBackArrow: true,
-        title: 'Category',
-       
-      ),
-      body: NestedScrollView(
-          headerSliverBuilder: (_, innerBoxIsScrolled) {
-            return [
-              SliverAppBar(
-                pinned: true,
-                floating: true,
-                backgroundColor: AkHelperFunctions.isDarkMode(context)
-                    ? AkColors.black
-                    : AkColors.white,
-                expandedHeight: 440,
-                flexibleSpace: Padding(
-                  padding: EdgeInsets.only(left: 8, right: 8, top: 24),
-                  child: ListView(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    children: [
-                      AkSectionHeading(
-                          title: 'Featured Brands', onPressed: () {}),
-                      const SizedBox(height: AkSizes.spaceBtwItems / 1.5),
-                      // * GridView for brands cards
+    return DefaultTabController(
+      length: 6,
+      child: Scaffold(
+        appBar: AkAppBar(showBackArrow: true, title: 'Category'),
+        body: NestedScrollView(
+            headerSliverBuilder: (_, innerBoxIsScrolled) {
+              return [
+                SliverAppBar(
+                  pinned: true,
+                  floating: true,
+                  backgroundColor: AkHelperFunctions.isDarkMode(context)
+                      ? AkColors.black
+                      : AkColors.white,
+                  expandedHeight: 440,
+                  flexibleSpace: Padding(
+                    padding: EdgeInsets.only(left: 8, right: 8, top: 24),
+                    child: ListView(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      children: [
+                        AkSectionHeading(
+                            title: 'Featured Brands', onPressed: () {}),
+                        const SizedBox(height: AkSizes.spaceBtwItems / 1.5),
+                        // * GridView for brands cards
 
-                      AkGridLayout(
-                          mainAxisExtent: 80,
-                          itemCount: 4,
-                          itemBuilder: (_, index) {
-                            return GestureDetector(
-                              onTap: () {},
-                              child: AkRoundedContainer(
-                                padding: EdgeInsets.all(AkSizes.sm),
-                                showBorder: true,
-                                backgroundColor: Colors.transparent,
-                                borderRadius: BorderRadius.circular(
-                                    AkSizes.borderRadiusLg),
-                                child: Row(
-                                  children: [
-                                    // * Icon
-                                    Flexible(
-                                        child: AkCircularImage(
-                                            image: AkImages.nikeLogo)),
-                                    const SizedBox(
-                                        height: AkSizes.spaceBtwItems),
-                                    // * Text
-
-                                    Expanded(
-                                      child: Column(
-                                        mainAxisSize: MainAxisSize.min,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          AkBrandTitleWithVerifiedIcon(
-                                              title: 'Nike',
-                                              brandTextSize: TextSizes.large),
-                                          Text('256 products',
-                                              overflow: TextOverflow.ellipsis,
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .labelMedium),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            );
-                          }),
+                        AkGridLayout(
+                            mainAxisExtent: 80,
+                            itemCount: 4,
+                            itemBuilder: (_, index) {
+                              return AkBrandCard(showBorder: true);
+                            }),
+                      ],
+                    ),
+                  ),
+                  // * Tabs
+                  bottom: AkTabBar(
+                    tabs: const [
+                      Tab(child: Text('Men')),
+                      Tab(child: Text('Women')),
+                      Tab(child: Text('Kids')),
+                      Tab(child: Text('Sports')),
+                      Tab(child: Text('Formal')),
+                      Tab(child: Text('Casual')),
                     ],
                   ),
                 ),
+              ];
+            },
+            body: TabBarView(children: const [
+              AkCategoryTab(
+                images: [
+                  AkImages.productImage2,
+                  AkImages.productImage3,
+                  AkImages.productImage4
+                ],
               ),
-            ];
-          },
-          body: Container()),
+              AkCategoryTab(
+                images: [
+                  AkImages.productImage11,
+                  AkImages.productImage10,
+                  AkImages.productImage9,
+                ],
+              ),
+              AkCategoryTab(images: [
+                AkImages.productImage5,
+                AkImages.productImage6,
+                AkImages.productImage7
+              ]),
+              AkCategoryTab(images: [
+                AkImages.productImage2,
+                AkImages.productImage8,
+                AkImages.productImage9
+              ]),
+              AkCategoryTab(images: [
+                AkImages.productImage2,
+                AkImages.productImage3,
+                AkImages.productImage4
+              ]),
+              AkCategoryTab(images: [
+                AkImages.productImage1,
+                AkImages.productImage11,
+                AkImages.productImage7
+              ]), 
+            ],
+            
+          )),
+      ),
     );
   }
 }
--- lib/features/shop/screens/category/widgets/category_tab.dart
+++ lib/features/shop/screens/category/widgets/category_tab.dart
@@ -1,25 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:shoes_store/common/widgets/layouts/grid_layout.dart';
+import 'package:shoes_store/common/widgets/products/product_cards/product_card_vertical.dart';
 
 import '../../../../../common/widgets/brand/brand_show_case.dart';
+import '../../../../../common/widgets/containers/section_cotainer.dart';
 import '../../../../../utils/constants/image_strings.dart';
 import '../../../../../utils/constants/sizes.dart';
 
 class AkCategoryTab extends StatelessWidget {
   const AkCategoryTab({
-    super.key, required this.images,
+    super.key,
+    required this.images,
   });
 
   final List<String> images;
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: const EdgeInsets.all(AkSizes.defaultSpace),
-      child: Column(
+    return ListView(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
         children: [
-          // * Brands
-          AkBrandShowcase(images: images),
-        ],
-      ),
-    );
+          Padding(
+            padding: const EdgeInsets.all(AkSizes.defaultSpace),
+            child: Column(
+              children: [
+                // * Brands
+                AkBrandShowcase(images: images),
+                const SizedBox(height: AkSizes.spaceBtwItems),
+
+                // * Products
+                AkSectionHeading(title: 'Featured Brands', onPressed: () {}),
+                const SizedBox(height: AkSizes.spaceBtwItems),
+
+                AkGridLayout(
+                    itemCount: 4,
+                    itemBuilder: (_, index) => const AkProductCardVertical(height: 150))
+              ],
+            ),
+          ),
+        ]);
   }
 }
