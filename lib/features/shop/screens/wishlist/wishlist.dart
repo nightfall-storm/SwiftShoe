249cfb755a7b5623006af0920010a8538a96c94c
nightfall <akka-bilal@outlook.com>
2024-09-24 17:02:54 +0100

    added WishList Screen

--- lib/features/shop/screens/wishlist/wishlist.dart
+++ lib/features/shop/screens/wishlist/wishlist.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:shoes_store/common/widgets/appbar/appbar.dart';
 
 class WishListScreen extends StatelessWidget {
   const WishListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      appBar: AkAppBar(showBackArrow: true),
+    );
   }
 }
