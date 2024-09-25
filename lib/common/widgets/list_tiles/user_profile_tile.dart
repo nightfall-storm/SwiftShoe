165a204a49051a171c8e0afe15c8f6fc7543710f
nightfall <akka-bilal@outlook.com>
2024-09-25 17:34:43 +0100

    Extracted widget of the profile header

---
+++ lib/common/widgets/list_tiles/user_profile_tile.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../utils/constants/colors.dart';
+import '../../../utils/constants/image_strings.dart';
+import '../../../utils/helpers/helper_functions.dart';
+import '../images/circular_image.dart';
+
+class AkUserProfileTile extends StatelessWidget {
+  const AkUserProfileTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: const AkCircularImage(
+        image: AkImages.user,
+        width: 50,
+        height: 50,
+        padding: 0,
+        applyColor: false,
+        applyOverlayColor: false,
+      ),
+      title: Text(
+        'Hello, nightfall',
+        style: Theme.of(context).textTheme.headlineSmall!.apply(
+            color: AkHelperFunctions.isDarkMode(context)
+                ? AkColors.white
+                : AkColors.black),
+      ),
+      subtitle: Text(
+        'night-fall@gmail.com',
+        style: Theme.of(context).textTheme.labelMedium!.apply(
+            color: AkHelperFunctions.isDarkMode(context)
+                ? AkColors.white
+                : AkColors.black),
+      ),
+      trailing: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Iconsax.edit,
+            color: AkHelperFunctions.isDarkMode(context)
+                ? AkColors.white
+                : AkColors.black,
+          )),
+    );
+  }
+}
