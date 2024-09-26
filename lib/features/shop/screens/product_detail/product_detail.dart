718c8c668342d0149246e5a806dbc0dcd817886b
nightfall <akka-bilal@outlook.com>
2024-09-26 17:03:47 +0100

    added Products Image Slider

--- lib/features/shop/screens/product_detail/product_detail.dart
+++ lib/features/shop/screens/product_detail/product_detail.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:shoes_store/common/widgets/Icon/circular_icon.dart';
 import 'package:shoes_store/common/widgets/appbar/appbar.dart';
 import 'package:shoes_store/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:shoes_store/common/widgets/images/round_image.dart';
 import 'package:shoes_store/utils/constants/colors.dart';
+import 'package:shoes_store/utils/constants/sizes.dart';
 import 'package:shoes_store/utils/helpers/helper_functions.dart';
 
 import '../../../../utils/constants/image_strings.dart';
+import 'widgets/product_detail_image_slider.dart';
 
 class ProductDetail extends StatelessWidget {
   const ProductDetail({super.key});
@@ -12,23 +17,57 @@ class ProductDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const AkAppBar(showBackArrow: true),
       body: SingleChildScrollView(
         child: Column(
           children: [
             // * Product Image Slider
-            AkCurvedEdges(
-                child: Container(
-              color: AkHelperFunctions.isDarkMode(context)
-                  ? AkColors.darkerGrey
-                  : AkColors.light,
-              child: const Stack(
+            const AkProductImageSlider(),
+
+            // * Product Details
+            Padding(
+              padding: const EdgeInsets.only(
+                  right: AkSizes.defaultSpace,
+                  left: AkSizes.defaultSpace,
+                  bottom: AkSizes.defaultSpace),
+              child: Column(
                 children: [
-                  // ? Main Large Image
-                  Image(image: AssetImage(AkImages.productImage9))
+                  // ? Rating & sharing
+                  Row(
+                    children: [
+                      // ? Rating
+                      Row(
+                        children: [
+                          const Icon(Iconsax.star5,
+                              color: Colors.amber, size: 24),
+                          const SizedBox(width: AkSizes.spaceBtwItems / 2),
+                          Text.rich(
+                            TextSpan(
+                              children: [
+                                TextSpan(
+                                    text: '5.0',
+                                    style:
+                                        Theme.of(context).textTheme.bodyLarge),
+                                const TextSpan(text: '(199)'),
+                              ],
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  )
+
+                  // ? Price, Title, Stock & Brand
+
+                  // ? Attributes
+
+                  // ? Checkout Button
+
+                  // ? Description
+
+                  // ? Reviews
                 ],
               ),
-            )),
+            ),
           ],
         ),
       ),
