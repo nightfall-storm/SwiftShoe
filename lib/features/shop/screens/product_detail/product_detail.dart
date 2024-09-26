d8e8a1ad68670f3b6237d0cd3dfb5ee9bbd7747e
nightfall <akka-bilal@outlook.com>
2024-09-26 20:03:52 +0100

    fixed imports and added page all details

--- lib/features/shop/screens/product_detail/product_detail.dart
+++ lib/features/shop/screens/product_detail/product_detail.dart
@@ -1,17 +1,12 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:shoes_store/common/widgets/Icon/circular_icon.dart';
-import 'package:shoes_store/common/widgets/appbar/appbar.dart';
-import 'package:shoes_store/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
-import 'package:shoes_store/common/widgets/images/round_image.dart';
+import 'package:readmore/readmore.dart';
+import 'package:shoes_store/common/widgets/containers/section_cotainer.dart';
+import 'package:shoes_store/features/shop/screens/product_detail/widgets/product_attributes.dart';
 import 'package:shoes_store/features/shop/screens/product_detail/widgets/product_meta_data.dart';
-import 'package:shoes_store/utils/constants/colors.dart';
 import 'package:shoes_store/utils/constants/sizes.dart';
-import 'package:shoes_store/utils/helpers/helper_functions.dart';
 
-import '../../../../utils/constants/image_strings.dart';
+import 'widgets/bottom_product_to_cart.dart';
 import 'widgets/product_detail_image_slider.dart';
 import 'widgets/rating_share.dart';
 
@@ -21,6 +16,7 @@ class ProductDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: const AkBottomAddToCart(),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -30,27 +26,62 @@ class ProductDetail extends StatelessWidget {
             // * Product Details
             Padding(
               padding: const EdgeInsets.only(
-                  right: AkSizes.defaultSpace,
-                  left: AkSizes.defaultSpace,
+                  right: AkSizes.defaultSpace - 7,
+                  left: AkSizes.defaultSpace - 10,
                   bottom: AkSizes.defaultSpace),
               child: Column(
                 children: [
                   // ? Rating & sharing
-                  AkRateAndShare(),
+                  const AkRateAndShare(),
 
                   // ? Price, Title, Stock & Brand
-                  AkProductMetaData(),
+                  const AkProductMetaData(),
 
                   // ? Attributes
-
+                  const AkProductAttributes(),
+                  const SizedBox(height: AkSizes.spaceBtwSections),
 
                   // ? Checkout Button
-
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        onPressed: () {}, child: const Text('Checkout')),
+                  ),
+                  const SizedBox(height: AkSizes.spaceBtwSections),
 
                   // ? Description
-
+                  const AkSectionHeading(title: 'Description'),
+                  const SizedBox(height: AkSizes.spaceBtwItems),
+                  const ReadMoreText(
+                    'Step into the future of comfort and performance with the Nike Air Zoom Runner in striking green. Engineered for athletes and trendsetters, this shoe features breathable mesh for ventilation and Zoom Air cushioning for unbeatable responsiveness. Perfect for everyday wear or intense workouts, the lightweight design offers flexibility while the durable outsole delivers excellent grip. Stand out in style with the bold green colorway, and experience the perfect blend of innovation, comfort, and iconic Nike design.',
+                    trimLines: 4,
+                    trimMode: TrimMode.Line,
+                    trimCollapsedText: ' Show More',
+                    trimExpandedText: ' Less',
+                    moreStyle: TextStyle(
+                        fontSize: 14, fontWeight: FontWeight.w800),
+                    lessStyle: TextStyle(
+                        fontSize: 14, fontWeight: FontWeight.w800),
+                  ),
 
                   // ? Reviews
+                  const Divider(),
+                  const SizedBox(height: AkSizes.spaceBtwItems),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      AkSectionHeading(
+                        title: 'Reviews(199)',
+                        onPressed: () {},
+                        showActionButton: false,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Iconsax.arrow_right_3, size: 18),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: AkSizes.spaceBtwSections),
                 ],
               ),
             ),
@@ -60,4 +91,3 @@ class ProductDetail extends StatelessWidget {
     );
   }
 }
-
