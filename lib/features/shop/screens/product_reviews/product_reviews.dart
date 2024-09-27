a88e403b39be6a603388f5f22961ffcdd828394e
nightfall <akka-bilal@outlook.com>
2024-09-27 15:30:53 +0100

    added product reviews and rating Screen

---
+++ lib/features/shop/screens/product_reviews/product_reviews.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:shoes_store/common/widgets/appbar/appbar.dart';
+
+class ProductReviewsScreen extends StatelessWidget {
+  const ProductReviewsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      // * AppBar
+      // * Body
+      body: SingleChildScrollView(
+        
+      ),
+    );
+  }
+}
