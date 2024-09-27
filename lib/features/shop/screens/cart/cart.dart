722ec0b1b43a99ce0c8d6460ef3047d2f6de06c0
nightfall <akka-bilal@outlook.com>
2024-09-27 20:50:07 +0100

    added Cart Screen

---
+++ lib/features/shop/screens/cart/cart.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../../common/widgets/appbar/appbar.dart';
+import '../../../../common/widgets/products/cart/add_remove_button.dart';
+
+import '../../../../common/widgets/texts/product_price_text.dart';
+import '../../../../utils/constants/sizes.dart';
+import '../../../../utils/helpers/navigation_helper.dart';
+import 'cart_item.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: deprecated_member_use
+    return WillPopScope(
+      onWillPop: () async {
+        handleBackNavigation(); // Call the reusable back navigation function
+        return false; // Prevent the default pop behavior
+      },
+      child: Scaffold(
+        appBar: AkAppBar(
+            title: 'Cart',
+            onPressed: () => Get.back(),
+            actions: const [],
+            appBarSize: 35),
+        body: Padding(
+          padding: const EdgeInsets.all(AkSizes.defaultSpace),
+          child: ListView.separated(
+              shrinkWrap: true,
+              itemCount: 10,
+              separatorBuilder: (_, __) =>
+                  const SizedBox(height: AkSizes.spaceBtwSections),
+              itemBuilder: (_, index) => const Column(
+                    children: [
+                      AkCartItem(),
+                      SizedBox(height: AkSizes.spaceBtwItems),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(width: 70),
+                              // * Add Remove Buttons
+                              AkProductQuantityWithAddRemove(),
+                            ],
+                          ),
+                          // * Product total price
+                          AkProductPriceText(price: '256'),
+                        ],
+                      )
+                    ],
+                  )),
+        ),
+        bottomNavigationBar: ElevatedButton(
+            onPressed: () {}, child: const Text('Checkout \$256.0')),
+      ),
+    );
+  }
+}
