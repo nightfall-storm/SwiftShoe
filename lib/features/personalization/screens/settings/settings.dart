48be886bfb039812f667fff2cb55c09cf9546cba
nightfall <akka-bilal@outlook.com>
2024-09-27 18:30:26 +0100

    added My Addresses onTap get to Addresses Screen

--- lib/features/personalization/screens/settings/settings.dart
+++ lib/features/personalization/screens/settings/settings.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:shoes_store/common/widgets/containers/section_cotainer.dart';
 import 'package:shoes_store/common/widgets/list_tiles/settings_menu_tile.dart';
+import 'package:shoes_store/features/personalization/screens/address/address.dart';
 
 import '../../../../common/widgets/appbar/appbar.dart';
 import '../../../../common/widgets/list_tiles/user_profile_tile.dart';
@@ -30,12 +32,8 @@ class SettingsScreen extends StatelessWidget {
                   // * Account Settings
                   AkSectionHeading(title: 'Account Settings', showActionButton: false),
                   const SizedBox(height: AkSizes.spaceBtwItems),
-                  AkSettingsMenuTile(
-                      icon: Iconsax.safe_home,
-                      title: "My Addresses",
-                      subtitle: 'Set shopping delivery address',
-                      onTap: () {}),
-                  AkSettingsMenuTile(icon: Iconsax.shopping_cart,title: 'My Cart', subtitle: 'Add, remove products and move to checkout',onTap: (){}),
+                  AkSettingsMenuTile(icon: Iconsax.safe_home, title: "My Addresses", subtitle: 'Set shopping delivery address', onTap: () => Get.to(const UserAddressScreen())),
+                  AkSettingsMenuTile(icon: Iconsax.shopping_cart,title: 'My Cart', subtitle: 'Add, remove products and move to checkout',onTap: () {}),
                   AkSettingsMenuTile(icon: Iconsax.bag_tick,title: 'My Orders', subtitle: 'In-Progress and Completed Orders',onTap: (){}),
                   AkSettingsMenuTile(icon: Iconsax.bank,title: 'Bank Account', subtitle: 'Withdraw balance to registered bank account',onTap: (){}),
                   AkSettingsMenuTile(icon: Iconsax.discount_shape,title: 'My Coupons', subtitle: 'List of all the discounted coupons',onTap: (){}),
