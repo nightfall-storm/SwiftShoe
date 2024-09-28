cfe5bf24b9330d42b1d9dedc0acf0510daadcfa7
nightfall <akka-bilal@outlook.com>
2024-09-28 18:49:40 +0100

    fixed get to CartScreen

--- lib/common/widgets/products/cart/cart_menu_icon.dart
+++ lib/common/widgets/products/cart/cart_menu_icon.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:shoes_store/features/shop/screens/cart/cart.dart';
 
 import '../../../../../utils/constants/colors.dart';
+import '../../../../navigation_menu.dart';
 
 class AkCartCounterIcon extends StatelessWidget {
   const AkCartCounterIcon({
@@ -19,7 +19,14 @@ class AkCartCounterIcon extends StatelessWidget {
       clipBehavior: Clip.none, // Allows overflow of the container
       children: [
         IconButton(
-          onPressed: () => Get.to(() => const CartScreen()),
+          onPressed: () {
+            // ? Set the selected index to 2 (CartScreen)
+            final navigationController = Get.find<NavigationController>();
+            navigationController.selectedIndex.value = 2;
+
+            // ? Navigate back to NavigationMenu
+            Get.to(() => const NavigationMenu());
+          },
           icon: Icon(Iconsax.shopping_bag,
               color: darkMode ? AkColors.white : AkColors.black),
         ),
--- lib/features/personalization/screens/settings/settings.dart
+++ lib/features/personalization/screens/settings/settings.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
@@ -10,6 +9,7 @@ import 'package:shoes_store/features/shop/screens/order/order.dart';
 
 import '../../../../common/widgets/appbar/appbar.dart';
 import '../../../../common/widgets/list_tiles/user_profile_tile.dart';
+import '../../../../navigation_menu.dart';
 import '../../../../utils/constants/sizes.dart';
 import '../../../../utils/helpers/navigation_helper.dart';
 
@@ -25,52 +25,94 @@ class SettingsScreen extends StatelessWidget {
         return false; // Prevent the default pop behavior
       },
       child: Scaffold(
-        appBar: AkAppBar(onAction: () {}, title: 'Settings',),
+        appBar: AkAppBar(
+          onAction: () {},
+          title: 'Settings',
+        ),
         body: SingleChildScrollView(
           child: Column(
             children: [
               // * User Profile card
               const AkUserProfileTile(),
-      
+
               // * Body
               Padding(
                 padding: const EdgeInsets.all(AkSizes.defaultSpace),
                 child: Column(
                   children: [
                     // * Account Settings
-                    const AkSectionHeading(title: 'Account Settings', showActionButton: false),
+                    const AkSectionHeading(
+                        title: 'Account Settings', showActionButton: false),
                     const SizedBox(height: AkSizes.spaceBtwItems),
-                    AkSettingsMenuTile(icon: Iconsax.safe_home, title: "My Addresses", subtitle: 'Set shopping delivery address', onTap: () => Get.to(() => const UserAddressScreen())),
-                    AkSettingsMenuTile(icon: Iconsax.shopping_cart,title: 'My Cart', subtitle: 'Add, remove products and move to checkout',onTap: () => Get.to(() => const CartScreen())),
-                    AkSettingsMenuTile(icon: Iconsax.bag_tick,title: 'My Orders', subtitle: 'In-Progress and Completed Orders',onTap: () => Get.to(() => const OrderScreen())),
-                    AkSettingsMenuTile(icon: Iconsax.bank,title: 'Bank Account', subtitle: 'Withdraw balance to registered bank account',onTap: (){}),
-                    AkSettingsMenuTile(icon: Iconsax.discount_shape,title: 'My Coupons', subtitle: 'List of all the discounted coupons',onTap: (){}),
-                    AkSettingsMenuTile(icon: Iconsax.notification1,title: 'Notifications', subtitle: 'Set any kind of notification message',onTap: (){}),
-                    AkSettingsMenuTile(icon: Iconsax.security_card,title: 'Account Privacy', subtitle: 'Manage data usage and connected accounts',onTap: (){}),
-      
+                    AkSettingsMenuTile(
+                        icon: Iconsax.safe_home,
+                        title: "My Addresses",
+                        subtitle: 'Set shopping delivery address',
+                        onTap: () => Get.to(() => const UserAddressScreen())),
+                    AkSettingsMenuTile(
+                        icon: Iconsax.shopping_cart,
+                        title: 'My Cart',
+                        subtitle: 'Add, remove products and move to checkout',
+                        onTap: () {
+                          // ? Set the selected index to 2 (CartScreen)
+                          final navigationController =
+                              Get.find<NavigationController>();
+                          navigationController.selectedIndex.value = 0;
+
+                          // ? Navigate back to NavigationMenu
+                          Get.to(() => const NavigationMenu());
+                        }),
+                    AkSettingsMenuTile(
+                        icon: Iconsax.bag_tick,
+                        title: 'My Orders',
+                        subtitle: 'In-Progress and Completed Orders',
+                        onTap: () => Get.to(() => const OrderScreen())),
+                    AkSettingsMenuTile(
+                        icon: Iconsax.bank,
+                        title: 'Bank Account',
+                        subtitle: 'Withdraw balance to registered bank account',
+                        onTap: () {}),
+                    AkSettingsMenuTile(
+                        icon: Iconsax.discount_shape,
+                        title: 'My Coupons',
+                        subtitle: 'List of all the discounted coupons',
+                        onTap: () {}),
+                    AkSettingsMenuTile(
+                        icon: Iconsax.notification1,
+                        title: 'Notifications',
+                        subtitle: 'Set any kind of notification message',
+                        onTap: () {}),
+                    AkSettingsMenuTile(
+                        icon: Iconsax.security_card,
+                        title: 'Account Privacy',
+                        subtitle: 'Manage data usage and connected accounts',
+                        onTap: () {}),
+
                     // * App Settings
                     const SizedBox(height: AkSizes.spaceBtwItems),
-                    const AkSectionHeading(title: 'App Settings', showActionButton: false),
+                    const AkSectionHeading(
+                        title: 'App Settings', showActionButton: false),
                     const SizedBox(height: AkSizes.spaceBtwItems),
-                    const AkSettingsMenuTile(icon: Iconsax.document_upload, title: 'Load Data', subtitle: 'Upload Data to your Cloud Firebase',),
-                    AkSettingsMenuTile(
-                      icon: Iconsax.location,
-                      title: 'GeoLocation',
-                      subtitle: 'Set recommendation based on location',
-                      trailing: Switch(value: true, onChanged: (value){})
+                    const AkSettingsMenuTile(
+                      icon: Iconsax.document_upload,
+                      title: 'Load Data',
+                      subtitle: 'Upload Data to your Cloud Firebase',
                     ),
                     AkSettingsMenuTile(
-                      icon: Iconsax.security_user,
-                      title: 'Safe Mode',
-                      subtitle: 'Search result is safe for all ages',
-                      trailing: Switch(value: false, onChanged: (value){})
-                    ),
+                        icon: Iconsax.location,
+                        title: 'GeoLocation',
+                        subtitle: 'Set recommendation based on location',
+                        trailing: Switch(value: true, onChanged: (value) {})),
                     AkSettingsMenuTile(
-                      icon: Iconsax.image,
-                      title: 'HD Image Quality',
-                      subtitle: 'Set image quality',
-                      trailing: Switch(value: false, onChanged: (value) {})
-                    ),
+                        icon: Iconsax.security_user,
+                        title: 'Safe Mode',
+                        subtitle: 'Search result is safe for all ages',
+                        trailing: Switch(value: false, onChanged: (value) {})),
+                    AkSettingsMenuTile(
+                        icon: Iconsax.image,
+                        title: 'HD Image Quality',
+                        subtitle: 'Set image quality',
+                        trailing: Switch(value: false, onChanged: (value) {})),
                   ],
                 ),
               )
