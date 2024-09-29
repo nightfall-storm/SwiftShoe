d632d73d853de85eb7ccb1ee23aca81fa697b6f9
nightfall <akka-bilal@outlook.com>
2024-09-29 17:33:44 +0100

    setup firebase and Initialize it

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:shoes_store/app.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  // * Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const App());
 }
