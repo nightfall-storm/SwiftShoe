3053337d6780a635bbd20f1a42491b269ee20abd
nightfall <akka-bilal@outlook.com>
2024-10-01 17:12:08 +0100

    corrected name

--- lib/utils/popups/full_screen_loader.dart
+++ lib/utils/popups/full_screen_loader.dart
@@ -5,7 +5,7 @@ import '../helpers/helper_functions.dart';
 import '../loaders/animation_loader.dart';
 
 /// A utility class for managing a full-screen loading dialog.
-class TFullScreenLoader {
+class AkFullScreenLoader {
   /// Open a full-screen loading dialog with a given text and animation.
   /// This method doesn't return anything.
   ///
