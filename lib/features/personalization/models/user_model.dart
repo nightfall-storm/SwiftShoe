d01ce0dd18c2d5811207bb19b990805b57cd3e8f
nightfall <akka-bilal@outlook.com>
2024-10-01 18:45:57 +0100

    Add UserModel class for Firebase integration
    - Implemented UserModel with properties: id, username, email, phoneNumber, and profilePicture.
    - Added methods for formatting phone numbers and generating usernames.
    - Included JSON serialization methods for compatibility with Firestore.
    - Created a factory method for constructing UserModel instances from Firestore snapshots.

---
+++ lib/features/personalization/models/user_model.dart
@@ -0,0 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../../utils/formatters/formatter.dart';
+
+class UserModel {
+  final String id;
+  final String username;
+  final String email;
+  String phoneNumber;
+  String profilePicture;
+
+  UserModel({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.phoneNumber,
+    required this.profilePicture,
+  });
+
+  String get formattedPhoneNumber => AkFormatter.formatPhoneNumber(phoneNumber);
+
+  static String generateUsername(String username) {
+    String camelCaseUsername = username.toLowerCase(); // Convert to lowercase
+    String usernameWithPrefix = "cwt_$camelCaseUsername"; // Add "cwt_" prefix
+    return usernameWithPrefix;
+  }
+
+  // Static function to create an empty user model.
+  static UserModel empty() => UserModel(
+      id: "", username: "", email: "", phoneNumber: "", profilePicture: "");
+
+  // Convert model to JSON structure for storing data in Firebase.
+  Map<String, dynamic> toJson() {
+    return {
+      'Username': username,
+      'Email': email,
+      'PhoneNumber': phoneNumber,
+      'ProfilePicture': profilePicture,
+    };
+  }
+
+  // Factory method to create a UserModel from a Firebase document snapshot.
+  factory UserModel.fromSnapshot(
+      DocumentSnapshot<Map<String, dynamic>> document) {
+    final data = document.data()!;
+    return UserModel(
+      id: document.id,
+      username: data['Username'] ?? "",
+      email: data['Email'] ?? "",
+      phoneNumber: data['PhoneNumber'] ?? "",
+      profilePicture: data['ProfilePicture'] ?? "",
+    );
+  }
+}
