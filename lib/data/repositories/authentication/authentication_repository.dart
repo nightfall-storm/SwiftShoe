0d13a60d79ac89891d0e15d2ec7b6ea3a597129d
nightfall <akka-bilal@outlook.com>
2024-10-01 18:47:53 +0100

    Enhance AuthenticationRepository with email authentication features
    - Integrated Firebase Authentication for user sign-up functionality.
    - Added registerWithEmailAndPassword method for user registration.
    - Included error handling for Firebase and platform exceptions.
    - Retained functionality for screen redirection based on first-time user status.
    - Improved structure for potential future email verification and password management features.

--- lib/data/repositories/authentication/authentication_repository.dart
+++ lib/data/repositories/authentication/authentication_repository.dart
@@ -1,14 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:shoes_store/features/authentication/screens/login/login.dart';
 import 'package:shoes_store/features/authentication/screens/onboarding/onboarding.dart';
+import 'package:shoes_store/utils/exceptions/firebase_auth_exceptions.dart';
+import 'package:shoes_store/utils/exceptions/firebase_exceptions.dart';
+import 'package:shoes_store/utils/exceptions/format_exceptions.dart';
+import 'package:shoes_store/utils/exceptions/platform_exceptions.dart';
 
 class AuthenticationRepository extends GetxController {
   static AuthenticationRepository get instance => Get.find();
 
   // * Variables
   final deviceStorage = GetStorage();
+  final _auth = FirebaseAuth.instance;
 
   // * Called from main.dart on app launch
   @override
@@ -29,4 +36,30 @@ class AuthenticationRepository extends GetxController {
       Get.offAll(() => const OnBoaridngScreen());
     }
   }
+
+/* ------------------------- Email & Password Sign-in ------------------------- */
+  // * [Email Authentication] - SignIn
+  
+  // * [Email Authentication] - Register
+  Future<UserCredential> registerWithEmailAndPassword(String email, String password) async{
+    try{
+      return await _auth.createUserWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e){
+      throw AkFirebaseAuthException(e.code).message;
+    } on FirebaseException catch (e){
+      throw AkFirebaseException(e.code).message;
+    } on FormatException catch (_){
+      throw const AkFormatException().message;
+    } on PlatformException catch (e){
+      throw AkPlatformException(e.code).message;
+    }catch(e){
+      throw 'Something went wrong. Please try again';
+    }
+  }
+  // * [Email Verification] - Mail Verification
+  // * [ReAuthentication] - ReAuthenticate User
+  // * [Email Authentication] - Forget Password
+  
+
+
 }
