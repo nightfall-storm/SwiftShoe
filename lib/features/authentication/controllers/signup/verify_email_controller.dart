8df8b0acfcf2c2b66583d5c1e8a73432233ef2ef
nightfall <akka-bilal@outlook.com>
2024-10-02 20:03:16 +0100

    Enhance VerifyEmailController with email verification logic
    - Implemented `sendEmailVerification` method that triggers an email verification link when the VerifyEmailScreen is initialized.
    - Added user feedback through snack bars for both successful and failed email verification attempts.

--- lib/features/authentication/controllers/signup/verify_email_controller.dart
+++ lib/features/authentication/controllers/signup/verify_email_controller.dart
@@ -1,18 +1,35 @@
 import 'package:get/get.dart';
+import 'package:shoes_store/utils/popups/loaders.dart';
 
-class VerifyEmailController extends GetxController{
+import '../../../../data/repositories/authentication/authentication_repository.dart';
+
+class VerifyEmailController extends GetxController {
   static VerifyEmailController get instance => Get.find();
 
   // * Send Email Whenever Verify Screen appears & Set Timer for auto redirect.
   @override
-  void onInit(){
+  void onInit() {
+    sendEmailVerification();
     super.onInit();
   }
-  
+
   // * Send Email Verification Link
+  sendEmailVerification() async {
+    try {
+      await AuthenticationRepository.instance.sendEmailVerification();
+      AkLoaders.errorSnackBar(
+        title: 'Verification Email Sent',
+        message: 'Please check your inbox for the verification link.',
+      );
+    } catch (e) {
+      AkLoaders.errorSnackBar(
+        title: 'Verification Failed',
+        message: 'Failed to send verification email: ${e.toString()}',
+      );
+    }
+  }
 
   // * Set Timer to automatically redirect on Email Verification
 
   // * Manually check if email verified
-
 }
