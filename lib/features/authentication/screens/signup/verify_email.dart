b9314311cbe8f9a794915947fa077beaea6daa25
nightfall <akka-bilal@outlook.com>
2024-10-02 20:01:44 +0100

    Modify VerifyEmailScreen to include email parameter
    - The screen now accepts an optional email parameter to display to the user.
    - Added VerifyEmailController for better state management and future features.
    - Updated the layout to show an illustration and relevant texts instead of the previous header.

--- lib/features/authentication/screens/signup/verify_email.dart
+++ lib/features/authentication/screens/signup/verify_email.dart
@@ -1,29 +1,36 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:shoes_store/utils/constants/sizes.dart';
+import 'package:shoes_store/features/authentication/controllers/signup/verify_email_controller.dart';
+
 
 import '../../../../common/widgets/success_screen/success_screen.dart';
 import '../../../../utils/constants/image_strings.dart';
+import '../../../../utils/constants/sizes.dart';
 import '../../../../utils/constants/text_strings.dart';
+import '../../../../utils/helpers/helper_functions.dart';
 import '../login/login.dart';
-import 'widgets/verify_email_header.dart';
 
 class VerifyEmailScreen extends StatelessWidget {
-  const VerifyEmailScreen({super.key});
+  const VerifyEmailScreen({
+    super.key,
+    this.email,
+  });
+
+  final String? email;
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(VerifyEmailController());
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         actions: [
           IconButton(
             icon: const Icon(CupertinoIcons.clear),
-            onPressed: () {
-              Get.offAll(() => const LoginScreen());
-            },
-          )
+            onPressed: () => Get.offAll(() => const LoginScreen()),
+          ),
         ],
       ),
       body: SingleChildScrollView(
@@ -32,7 +39,20 @@ class VerifyEmailScreen extends StatelessWidget {
           child: Column(
             children: [
               // * Image & Title & SubTitle
-              const VerifyEmailHeader(),
+              Image.asset(
+                AkImages.deliveredEmailIllustration,
+                width: AkHelperFunctions.screenWidth() * 0.6,
+              ),
+              const SizedBox(height: AkSizes.spaceBtwItems),
+              Text(AkTexts.confirmEmail,
+                  style: Theme.of(context).textTheme.headlineMedium,
+                  textAlign: TextAlign.center),
+              Text(email ?? '',
+                  style: Theme.of(context).textTheme.labelLarge,
+                  textAlign: TextAlign.center),
+              Text(AkTexts.confirmEmailSubTitle,
+                  style: Theme.of(context).textTheme.labelMedium,
+                  textAlign: TextAlign.center),
               const SizedBox(height: AkSizes.spaceBtwSections),
               // * Buttons
               SizedBox(
