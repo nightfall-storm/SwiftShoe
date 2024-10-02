69e1c9cb6539ffb3e087f3aa183f8a938e5cd2aa
nightfall <akka-bilal@outlook.com>
2024-10-02 19:43:38 +0100

    added email verification Controller

---
+++ lib/features/authentication/controllers/signup/verify_email_controller.dart
@@ -0,0 +1,18 @@
+import 'package:get/get.dart';
+
+class VerifyEmailController extends GetxController{
+  static VerifyEmailController get instance => Get.find();
+
+  // * Send Email Whenever Verify Screen appears & Set Timer for auto redirect.
+  @override
+  void onInit(){
+    super.onInit();
+  }
+  
+  // * Send Email Verification Link
+
+  // * Set Timer to automatically redirect on Email Verification
+
+  // * Manually check if email verified
+
+}
