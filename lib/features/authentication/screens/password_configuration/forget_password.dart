7ad6f06e00afa1755c217f55bd48cf579bfed1c3
nightfall <akka-bilal@outlook.com>
2024-10-04 21:03:17 +0100

    Enhance ForgetPassword screen with email input, form validation, and submit button functionality

--- lib/features/authentication/screens/password_configuration/forget_password.dart
+++ lib/features/authentication/screens/password_configuration/forget_password.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:shoes_store/utils/constants/sizes.dart';
 import '../../../../common/styles/spacing_style.dart';
+import '../../../../utils/constants/sizes.dart';
+import '../../../../utils/constants/text_strings.dart';
+import '../../../../utils/validators/validation.dart';
+import '../../controllers/forget_password/forget_password_controller.dart';
 import '../login/login.dart';
-import 'widgets/forget_password_form.dart';
 import 'widgets/forget_password_header.dart';
 
 class ForgetPassword extends StatelessWidget {
@@ -12,31 +14,56 @@ class ForgetPassword extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            icon: const Icon(CupertinoIcons.clear),
-            onPressed: () {
-              Get.offAll(() => const LoginScreen());
-            },
-          )
-        ],
-      ),
-      body: const SingleChildScrollView(
-        child: Padding(
-          padding: AkSpacingStyle.paddingWithAppBarHeight,
-          child: Column(
-            children: [
-              ForgetPasswordHeader(),
-              SizedBox(height: AkSizes.spaceBtwSections / 2),
-              ForgetPasswordForm(),
-            ],
+    final controller = Get.put(ForgetPasswordController());
+    // ignore: deprecated_member_use
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+              icon: const Icon(CupertinoIcons.clear),
+              onPressed: () {
+                Get.offAll(() => const LoginScreen());
+              },
+            )
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: AkSpacingStyle.paddingWithAppBarHeight,
+            child: Column(
+              children: [
+                const ForgetPasswordHeader(),
+                const SizedBox(height: AkSizes.spaceBtwSections / 2),
+                Form(
+                  key: controller.forgetPasswordFormKey,
+                  child: TextFormField(
+                    controller: controller.email,
+                    validator: AkValidator.validateEmail,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.email),
+                      labelText: AkTexts.email,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: AkSizes.spaceBtwSections),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () => controller.sendPasswordResetEmail(),
+                    child: const Text(AkTexts.submit),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-
