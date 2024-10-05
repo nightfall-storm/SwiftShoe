a071afe11b6b6b974cdc3631c55f5b07fa1de56e
nightfall <akka-bilal@outlook.com>
2024-10-05 16:40:01 +0100

    Add reactive user state management and user data fetching in UserController - Introduced Rx<UserModel> to manage reactive user state. - Implemented onInit lifecycle method to automatically fetch user data on initialization. - Added fetchUserRecord method to retrieve user details from Firestore and update the reactive user state."

--- lib/features/personalization/controllers/user_controller.dart
+++ lib/features/personalization/controllers/user_controller.dart
@@ -8,7 +8,23 @@ import '../models/user_model.dart';
 class UserController extends GetxController {
   static UserController get instance => Get.find();
 
+  Rx<UserModel> user = UserModel.empty().obs;
   final userRepository = Get.put(UserRepository());
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchUserRecord();
+  }
+
+  Future<void> fetchUserRecord() async {
+    try {
+      final user = await userRepository.fetchUserDetails();
+      this.user(user);
+    } catch (e) {
+      user(UserModel.empty());
+    }
+  }
 
   // * Save user record from any registration provider
   Future<void> saveUserRecord(UserCredential? userCredentials) async {
