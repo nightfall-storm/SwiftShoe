cb30e922e1883ebb8e0cfc03b541e48f84289412
nightfall <akka-bilal@outlook.com>
2024-10-05 19:15:54 +0100

    Enhance UserController with account deletion and re-authentication functionality
    - Added deleteAccountWarningPopup method to confirm account deletion.
    - Implemented deleteUserAccount method to handle user account deletion with re-authentication.
    - Introduced reAuthenticateEmailAndPasswordUser method for email/password re-authentication.
    - Integrated network connectivity checks before re-authentication.
    - Added loading indicators during processing operations.
    - Updated imports and cleaned up code structure.

--- lib/features/personalization/controllers/user_controller.dart
+++ lib/features/personalization/controllers/user_controller.dart
@@ -1,16 +1,29 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shoes_store/data/repositories/authentication/authentication_repository.dart';
 import 'package:shoes_store/data/repositories/user/user_repository.dart';
+import 'package:shoes_store/features/authentication/screens/login/login.dart';
+import 'package:shoes_store/utils/helpers/network_manager.dart';
+import 'package:shoes_store/utils/popups/full_screen_loader.dart';
 import 'package:shoes_store/utils/popups/loaders.dart';
 
+import '../../../utils/constants/image_strings.dart';
+import '../../../utils/constants/sizes.dart';
 import '../models/user_model.dart';
+import '../screens/profile/widgets/re_authenticate_user_login_form.dart';
 
 class UserController extends GetxController {
   static UserController get instance => Get.find();
 
   final profileLoading = false.obs;
   Rx<UserModel> user = UserModel.empty().obs;
+
+  final hidePassword = true.obs;
+  final verifyEmail = TextEditingController();
+  final verifyPassword = TextEditingController();
   final userRepository = Get.put(UserRepository());
+  GlobalKey<FormState> reAuthFormKey = GlobalKey<FormState>();
 
   @override
   void onInit() {
@@ -25,7 +38,7 @@ class UserController extends GetxController {
       this.user(user);
     } catch (e) {
       user(UserModel.empty());
-    }finally{
+    } finally {
       profileLoading.value = false;
     }
   }
@@ -55,6 +68,90 @@ class UserController extends GetxController {
         message:
             'Something went wrong while saving your information. You can re-save your data in your profile',
       );
+    }
+  }
+
+  // * Delete User Account Warning
+  void deleteAccountWarningPopup() {
+    Get.defaultDialog(
+      contentPadding: const EdgeInsets.all(AkSizes.md),
+      title: 'Delete Account',
+      middleText:
+          'Are you sure you want to delete your account permanently?, This action is not reversible and all of your data will be removed permanently.',
+      confirm: ElevatedButton(
+        onPressed: () async => deleteUserAccount(),
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            side: const BorderSide(color: Colors.red)),
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: AkSizes.lg),
+          child: Text('Delete'),
+        ),
+      ),
+      cancel: OutlinedButton(
+        child: const Text('Cancel'),
+        onPressed: () => Navigator.of(Get.overlayContext!).pop(),
+      ),
+    );
+  }
+
+  // * Delete User Account
+  void deleteUserAccount() async {
+    try {
+      AkFullScreenLoader.openLoadingDialog(
+          'Precessing...', AkImages.docerAnimation);
+
+      // First re-authenticate user
+      final auth = AuthenticationRepository.instance;
+      final provider =
+          auth.authUser!.providerData.map((e) => e.providerId).first;
+      if (provider.isNotEmpty) {
+        // Re Verify Auth Email
+        if (provider == 'google.com') {
+          await auth.signInWithGoogle();
+          await auth.deleteAccount();
+          AkFullScreenLoader.stopLoading();
+          Get.offAll(() => const LoginScreen());
+        } else if (provider == 'password') {
+          AkFullScreenLoader.stopLoading();
+          Get.to(() => const ReAuthLoginForm());
+        }
+      }
+    } catch (e) {
+      AkFullScreenLoader.stopLoading();
+      AkLoaders.warningSnackBar(title: 'Oh Snap!', message: e.toString());
+    }
+  }
+
+  // * Re-Authenticate before deleting
+  Future<void> reAuthenticateEmailAndPasswordUser() async {
+    try {
+      if (!reAuthFormKey.currentState!.validate()) {
+        return;
+      }
+      
+      AkFullScreenLoader.openLoadingDialog(
+          'Processing...', AkImages.docerAnimation);
+
+      // Check Network Connectivity
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        AkFullScreenLoader.stopLoading();
+        AkLoaders.errorSnackBar(
+            title: 'No Internet',
+            message: 'Please check your internet connection');
+        return;
+      }
+
+      await AuthenticationRepository.instance
+          .reAuthenticateWithEmailAndPassword(
+              verifyEmail.text.trim(), verifyPassword.text.trim());
+      await AuthenticationRepository.instance.deleteAccount();
+      AkFullScreenLoader.stopLoading();
+      Get.offAll(() => const LoginScreen());
+    } catch (e) {
+      AkFullScreenLoader.stopLoading();
+      AkLoaders.warningSnackBar(title: 'Oh Snap!', message: e.toString());
     }
   }
 }
