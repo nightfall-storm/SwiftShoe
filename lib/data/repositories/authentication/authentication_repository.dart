9cd34b89eebf34f4ae2e43d9f0641fbf753b9f23
nightfall <akka-bilal@outlook.com>
2024-10-05 16:36:54 +0100

    Get authenticated UserData

--- lib/data/repositories/authentication/authentication_repository.dart
+++ lib/data/repositories/authentication/authentication_repository.dart
@@ -22,6 +22,9 @@ class AuthenticationRepository extends GetxController {
   final deviceStorage = GetStorage();
   final _auth = FirebaseAuth.instance;
 
+  // Get Authenticated User Data
+  User? get authUser => _auth.currentUser;
+
   // * Called from main.dart on app launch
   @override
   void onReady() {
