8d9deb86cec75851cb323cf701586cbb16211f44
nightfall <akka-bilal@outlook.com>
2024-10-13 22:33:06 +0100

    removed prints, added snackbar error

--- lib/data/repositories/brands/brand_repository.dart
+++ lib/data/repositories/brands/brand_repository.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:shoes_store/utils/exceptions/firebase_exceptions.dart';
 import 'package:shoes_store/utils/exceptions/platform_exceptions.dart';
+import 'package:shoes_store/utils/popups/loaders.dart';
 
 import '../../../features/shop/models/brand_model.dart';
 import '../../services/firebase_storage_service.dart';
@@ -38,24 +39,19 @@ class BrandRepository extends GetxController {
 
       for (var brand in brands) {
         try {
-          print('Uploading brand: ${brand.name}');
-
           // Get ImageData link from the local assets
           final file = await storage.getImageDataFromAssets(brand.image);
-          print('Image data retrieved for ${brand.name}');
 
           // Upload Image and Get its URL
           final url = await storage.uploadImageData('Brands', file, brand.name);
-          print('Image uploaded for ${brand.name}');
 
           // Assign URL to Brand.image attribute
           brand.image = url;
 
           // Store Brand in Firestore
           await _db.collection("Brands").doc(brand.id).set(brand.toJson());
-          print('Brand data uploaded to Firestore for ${brand.name}');
         } catch (e) {
-          print('Error uploading brand ${brand.name}: $e');
+          AkLoaders.errorSnackBar(title: 'Oh Snap!', message: e.toString());
         }
       }
     } on FirebaseException catch (e) {
