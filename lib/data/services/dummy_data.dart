7976ec960cdcdf28b059bd7a61b3e6112a59c6a8
nightfall <akka-bilal@outlook.com>
2024-10-13 22:36:59 +0100

    feat: Implement uploadDummyBrandsAndCollections method for dummy data upload

--- lib/data/services/dummy_data.dart
+++ lib/data/services/dummy_data.dart
@@ -1,54 +1,95 @@
-import 'package:get/get.dart';
-import 'package:shoes_store/features/shop/models/brand_model.dart';
-import 'package:shoes_store/data/repositories/brands/brand_repository.dart';
+import 'package:logger/logger.dart';
+import 'package:shoes_store/features/shop/models/collection_model.dart';
+import 'package:shoes_store/data/repositories/collections/collection_repository.dart';
 
 class AkDummyData {
-  static Future<void> uploadDummyBrands() async {
-    final brands = [
-      BrandModel(
-        id: 'brand1',
-        name: 'Nike',
-        image: 'assets/icons/brands/nike.png',
+  static final Logger logger = Logger();
+  static Future<void> uploadDummyBrandsAndCollections() async {
+    // final brands = [
+    //   BrandModel(
+    //     id: 'nike',
+    //     name: 'Nike',
+    //     image: 'assets/icons/brands/nike.png',
+    //     isFeatured: true,
+    //   ),
+    //   BrandModel(
+    //     id: 'adidas',
+    //     name: 'Adidas',
+    //     image: 'assets/icons/brands/adidas-logo.png',
+    //     isFeatured: true,
+    //   ),
+    //   BrandModel(
+    //     id: 'puma',
+    //     name: 'Puma',
+    //     image: 'assets/icons/brands/puma-logo.png',
+    //     isFeatured: true,
+    //   ),
+    //   BrandModel(
+    //     id: 'brooks',
+    //     name: 'Brooks',
+    //     image: 'assets/icons/brands/brooks-logo.png',
+    //     isFeatured: true,
+    //   ),
+    //   BrandModel(
+    //     id: 'jordan',
+    //     name: 'Jordan',
+    //     image: 'assets/icons/brands/jordan-logo.png',
+    //     isFeatured: true,
+    //   ),
+    //   BrandModel(
+    //     id: 'crocs',
+    //     name: 'Crocs',
+    //     image: 'assets/icons/brands/crocs-logo.png',
+    //     isFeatured: true,
+    //   ),
+    // ];
+
+    final collections = [
+      CollectionModel(
+        id: 'men',
+        name: 'Men',
         isFeatured: true,
       ),
-      BrandModel(
-        id: 'brand2',
-        name: 'Adidas',
-        image: 'assets/icons/brands/adidas-logo.png',
+      CollectionModel(
+        id: 'women',
+        name: 'Women',
         isFeatured: true,
       ),
-      BrandModel(
-        id: 'brand3',
-        name: 'Puma',
-        image: 'assets/icons/brands/puma-logo.png',
+      CollectionModel(
+        id: 'kids',
+        name: 'Kids',
+        isFeatured: false,
+      ),
+      CollectionModel(
+        id: 'sports',
+        name: 'Sports',
         isFeatured: true,
       ),
-      BrandModel(
-        id: 'brand4',
-        name: 'Brooks',
-        image: 'assets/icons/brands/brooks-logo.png',
+      CollectionModel(
+        id: 'formal',
+        name: 'Formal',
         isFeatured: true,
       ),
-      BrandModel(
-        id: 'brand5',
-        name: 'Jordan',
-        image: 'assets/icons/brands/jordan-logo.png',
-        isFeatured: true,
-      ),
-      BrandModel(
-        id: 'brand6',
-        name: 'Crocs',
-        image: 'assets/icons/brands/crocs-logo.png',
+      CollectionModel(
+        id: 'casual',
+        name: 'Casual',
         isFeatured: true,
       ),
     ];
 
     try {
-      // Upload the dummy data to Firebase
-      await BrandRepository.instance.uploadDummyData(brands);
-      Get.snackbar('Success', 'Dummy brands uploaded successfully');
+      // Upload the dummy brands data to Firebase
+      // await BrandRepository.instance.uploadDummyData(brands);
+      // Get.snackbar('Success', 'Dummy brands uploaded successfully');
+
+      // Upload the dummy collections data to Firebase
+      await CollectionRepository.instance.uploadDummyData(collections);
+
+      // Log success message
+      logger.i('Success: Dummy collections uploaded successfully');
     } catch (e) {
-      Get.snackbar('Error', e.toString());
+      // Log error message
+      logger.e('Error: $e');
     }
   }
 }
