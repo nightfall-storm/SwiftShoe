26580b8840f24e5e3bad3527d6b378f9e4483f07
nightfall <akka-bilal@outlook.com>
2024-10-14 19:54:19 +0100

    implement static images for now for showCase

--- lib/features/shop/screens/category/widgets/category_tab.dart
+++ lib/features/shop/screens/category/widgets/category_tab.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:shoes_store/common/widgets/layouts/grid_layout.dart';
 import 'package:shoes_store/common/widgets/products/product_cards/product_card_vertical.dart';
+import 'package:shoes_store/features/shop/models/collection_model.dart';
 
 import '../../../../../common/widgets/brand/brand_show_case.dart';
 import '../../../../../common/widgets/containers/section_cotainer.dart';
+import '../../../../../utils/constants/image_strings.dart';
 import '../../../../../utils/constants/sizes.dart';
 
 class AkCategoryTab extends StatelessWidget {
   const AkCategoryTab({
-    super.key,
-    required this.images,
+    super.key, required this.collection,
   });
+  final CollectionModel collection;
 
-  final List<String> images;
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -24,7 +25,11 @@ class AkCategoryTab extends StatelessWidget {
             child: Column(
               children: [
                 // * Brands
-                AkBrandShowcase(images: images),
+                const AkBrandShowcase(images: [
+                  AkImages.productImage1,
+                  AkImages.productImage2,
+                  AkImages.productImage3,
+                ]),
                 const SizedBox(height: AkSizes.spaceBtwItems),
 
                 // * Products
@@ -33,7 +38,8 @@ class AkCategoryTab extends StatelessWidget {
 
                 AkGridLayout(
                     itemCount: 4,
-                    itemBuilder: (_, index) => const AkProductCardVertical(height: 150))
+                    itemBuilder: (_, index) =>
+                        const AkProductCardVertical(height: 150))
               ],
             ),
           ),
