4bf8d02a3ba792e78631ad1c5617b75395bbbc63
nightfall <akka-bilal@outlook.com>
2024-10-16 00:16:05 +0100

    Integrate ProductModel and ProductController into AkProductCardVertical
    - Added dynamic product data rendering using ProductModel.
    - Implemented sale percentage calculation using ProductController.
    - Updated product thumbnail, price, and brand display.
    - Added conditional rendering for sale price and original price.

--- lib/common/widgets/products/product_cards/product_card_vertical.dart
+++ lib/common/widgets/products/product_cards/product_card_vertical.dart
@@ -1,34 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:shoes_store/features/shop/controllers/product_controller.dart';
 
-import 'package:shoes_store/common/styles/shadows_style.dart';
-import 'package:shoes_store/common/widgets/containers/rounded_container.dart';
-import 'package:shoes_store/common/widgets/images/round_image.dart';
-import 'package:shoes_store/features/shop/screens/product_detail/product_detail.dart';
-import 'package:shoes_store/utils/constants/image_strings.dart';
-import 'package:shoes_store/utils/helpers/helper_functions.dart';
-
+import '../../../../features/shop/models/product_model.dart';
+import '../../../../features/shop/screens/product_detail/product_detail.dart';
 import '../../../../utils/constants/colors.dart';
+import '../../../../utils/constants/enums.dart';
 import '../../../../utils/constants/sizes.dart';
+import '../../../../utils/helpers/helper_functions.dart';
+import '../../../styles/shadows_style.dart';
 import '../../Icon/circular_icon.dart';
+import '../../containers/rounded_container.dart';
+import '../../images/round_image.dart';
 import '../../texts/brand_title_verified_icon.dart';
 import '../../texts/product_price_text.dart';
 import '../../texts/product_title_text.dart';
 
 class AkProductCardVertical extends StatelessWidget {
   const AkProductCardVertical(
-      {super.key, this.height = 180, this.width = 180, this.imageHeight = 180});
+      {super.key,
+      this.height = 180,
+      this.width = 180,
+      this.imageHeight = 180,
+      required this.product});
 
   final double height;
   final double imageHeight;
   final double width;
 
+  final ProductModel product;
+
   @override
   Widget build(BuildContext context) {
+    final controller = ProductController.instance;
+    final salePercentage =
+        controller.calculateSalePercentage(product.price, product.salePrice);
     final isDarkMode = AkHelperFunctions.isDarkMode(context);
     return GestureDetector(
-      onTap: () => Get.to(() => const ProductDetail()),
+      onTap: () => Get.to(() => ProductDetail(
+            product: product,
+          )),
       child: Container(
         width: width,
         height: height,
@@ -44,13 +56,14 @@ class AkProductCardVertical extends StatelessWidget {
               children: [
                 // * Thumbnail Image
                 AkRoundedImage(
-                  imageUrl: AkImages.productImage1,
+                  imageUrl: product.thumbnail,
                   height: imageHeight,
+                  isNetworkImage: true,
                   backgroundColor: AkColors.lightContainer,
                 ),
                 // * Discount TAG
                 Positioned(
-                  top: 17,
+                  top: 7,
                   left: 3,
                   child: Transform.rotate(
                     angle: -3.14 / 2,
@@ -67,7 +80,7 @@ class AkProductCardVertical extends StatelessWidget {
                         vertical: AkSizes.xs,
                       ),
                       child: Text(
-                        '30% OFF',
+                        '$salePercentage%',
                         style: Theme.of(context)
                             .textTheme
                             .labelLarge!
@@ -90,16 +103,17 @@ class AkProductCardVertical extends StatelessWidget {
             ),
             const SizedBox(height: AkSizes.spaceBtwItems / 2),
             // * Details Section
-            const Padding(
-              padding: EdgeInsets.only(left: AkSizes.sm),
+            Padding(
+              padding: const EdgeInsets.only(left: AkSizes.sm),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  AkProductTitleText(
-                      title: 'Green Nike Air Shoes', smallSize: true),
-                  SizedBox(height: AkSizes.spaceBtwItems / 2),
+                  AkProductTitleText(title: product.title, smallSize: true),
+                  const SizedBox(height: AkSizes.spaceBtwItems / 2),
                   AkBrandTitleWithVerifiedIcon(
-                      title: 'Nike',
+                      title: product.brand != null
+                          ? product.brand!.name
+                          : 'Unknown Brand',
                       textAlign: TextAlign.start,
                       mainAxisSize: MainAxisSize.max),
                 ],
@@ -111,11 +125,35 @@ class AkProductCardVertical extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Padding(
-                  padding: EdgeInsets.only(left: AkSizes.sm),
-                  child: AkProductPriceText(
-                    price: '35.0',
-                    isLarge: true,
+                // * Price
+                Flexible(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: AkSizes.sm),
+                          child: AkProductPriceText(
+                            price: controller.getProductPrice(product),
+                            isLarge: true,
+                          ),
+                        ),
+                        if (product.productType ==
+                                ProductType.single.toString() &&
+                            product.salePrice > 0)
+                          Padding(
+                            padding: const EdgeInsets.only(left: AkSizes.sm),
+                            child: Text(
+                              '${product.price}\$',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelMedium!
+                                  .apply(
+                                      decoration: TextDecoration.lineThrough),
+                            ),
+                          ),
+                      ],
+                    ),
                   ),
                 ),
                 Container(
