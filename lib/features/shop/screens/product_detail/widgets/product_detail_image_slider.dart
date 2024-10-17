19b5424243ced1030b847e5dcde8c7751822b53f
nightfall <akka-bilal@outlook.com>
2024-10-17 21:08:49 +0100

    Add dynamic product image slider with thumbnails and enlarged image view

--- lib/features/shop/screens/product_detail/widgets/product_detail_image_slider.dart
+++ lib/features/shop/screens/product_detail/widgets/product_detail_image_slider.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
@@ -7,32 +8,47 @@ import '../../../../../common/widgets/appbar/appbar.dart';
 import '../../../../../common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 import '../../../../../common/widgets/images/round_image.dart';
 import '../../../../../utils/constants/colors.dart';
-import '../../../../../utils/constants/image_strings.dart';
 import '../../../../../utils/constants/sizes.dart';
 import '../../../../../utils/helpers/helper_functions.dart';
+import '../../../controllers/product/images_controller.dart';
+import '../../../models/product_model.dart';
 
 class AkProductImageSlider extends StatelessWidget {
-  const AkProductImageSlider({
-    super.key,
-  });
+  const AkProductImageSlider({super.key, required this.product});
+  final ProductModel product;
 
   @override
   Widget build(BuildContext context) {
     final bool isDarkMode = AkHelperFunctions.isDarkMode(context);
+
+    final controller = Get.put(ImagesController());
+    final images = controller.getAllProductImages(product);
+
     return AkCurvedEdges(
         child: Container(
       color: isDarkMode ? AkColors.darkContainer : AkColors.light,
       child: Stack(
         children: [
           // ? Main Large Image
-          const SizedBox(
+          SizedBox(
             height: 400,
             child: Padding(
-              padding: EdgeInsets.all(AkSizes.productImageRadius * 2),
+              padding: const EdgeInsets.all(AkSizes.productImageRadius * 2),
               child: Center(
-                child: Image(
-                  image: AssetImage(AkImages.productImage1),
-                ),
+                child: Obx(() {
+                  final image = controller.selectedProductImage.value;
+                  return GestureDetector(
+                    onTap: () => controller.showEnLargedImage(image),
+                    child: CachedNetworkImage(
+                      imageUrl: image,
+                      progressIndicatorBuilder: (_, __, downloadProgress) =>
+                          CircularProgressIndicator(
+                        value: downloadProgress.progress,
+                        color: AkColors.primary,
+                      ),
+                    ),
+                  );
+                }),
               ),
             ),
           ),
@@ -48,14 +64,23 @@ class AkProductImageSlider extends StatelessWidget {
                 separatorBuilder: (_, __) => const SizedBox(
                   width: AkSizes.spaceBtwItems,
                 ),
-                itemCount: 6,
-                itemBuilder: (_, index) => AkRoundedImage(
-                    width: 80,
-                    backgroundColor:
-                        isDarkMode ? AkColors.darkContainer : AkColors.white,
-                    border: Border.all(color: AkColors.primary),
-                    padding: const EdgeInsets.all(AkSizes.sm),
-                    imageUrl: AkImages.productImage3),
+                itemCount: images.length,
+                itemBuilder: (_, index) => Obx(() {
+                  final imageSelected =
+                      controller.selectedProductImage.value == images[index];
+                  return AkRoundedImage(
+                      isNetworkImage: true,
+                      width: 80,
+                      backgroundColor:
+                          isDarkMode ? AkColors.dark : AkColors.white,
+                      border: Border.all(
+                          color: imageSelected
+                              ? AkColors.primary
+                              : Colors.transparent),
+                      onPressed: () => controller.selectedProductImage.value = images[index],
+                      padding: const EdgeInsets.all(AkSizes.sm),
+                      imageUrl: images[index]);
+                }),
               ),
             ),
           ),
